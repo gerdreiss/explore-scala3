1f13b50f763988fd7c8edb751cf3c2d138c0e74c
Gerd Reiss <gerd@reiss.pro>
2026-06-16 07:26:28 +0200

    Exploring Scala 3 - print person's age using extension

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,12 +5,12 @@ import java.lang.Double as JDouble
 @main def hello(): Unit =
 
   Person("Mike", LocalDate.of(2020, 1, 1)) match
-    case Child(name)     => println(s"$name is a child")
-    case Teenager(name)  => println(s"$name is a teenager")
-    case Adult(name)     => println(s"$name is an adult")
-    case Retired(name)   => println(s"$name is probably retired")
-    case Dead(name)      => println(s"$name is probably dead")
-    case Person(name, _) => println(s"$name is not born yet")
+    case p @ Child(name)    => println(s"$name is a ${p.age} years old child")
+    case p @ Teenager(name) => println(s"$name is a ${p.age} years old teenager")
+    case p @ Adult(name)    => println(s"$name is a ${p.age} years old adult")
+    case p @ Retired(name)  => println(s"$name is ${p.age} years old and probably retired")
+    case p @ Dead(name)     => println(s"$name would be ${p.age} years old and probably dead")
+    case Person(name, _)    => println(s"$name is not born yet")
 
   val r = Random()
   val n =
